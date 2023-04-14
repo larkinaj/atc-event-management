2d9c5a78bae71d9db86717cbcfba2c3ea95b09f4
larkinaj <larkin.aj@gmail.com>
2023-04-13 21:22:21 -0400

    event creation page complete

--- src/client/App.js
+++ src/client/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import Dashboard from "./Dashboard";
 import ProfilePage from "./ProfilePage";
+import CreateEvent from "./CreateEvent";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/create-event" element={<CreateEvent />} />
             </Routes>
         </div>
     )
