820819a7eda49e7b846caf059e3653d247069feb
Kareem Saleh <kareemhs@gmail.com>
2023-04-13 21:46:28 -0400

    fix postgres URI

--- src/server/models/appModel.js
+++ src/server/models/appModel.js
@@ -1,8 +1,8 @@
 // pg model for blessing 
 import pg from 'pg';
 
-// const PG_URI = "postgres://wbpifxvw:q8lghllZohqY4rkyYdpyF64er5iguO96@heffalump.db.elephantsql.com/wbpifxvw"
-const PG_URI = "postgres://lxigtjgl:wP50ncLWiiuuLCbVnZdsgx2XaV5ar3tr@lallah.db.elephantsql.com/lxigtjgl"
+const PG_URI = "postgres://wbpifxvw:q8lghllZohqY4rkyYdpyF64er5iguO96@heffalump.db.elephantsql.com/wbpifxvw"
+
 
 const pool = new pg.Pool({
   connectionString: PG_URI
