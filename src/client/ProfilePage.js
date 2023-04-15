888d4ca7f5b99422f417b765d929dc0d07fb1c1c
larkinaj <larkin.aj@gmail.com>
2023-04-14 20:58:47 -0400

    Put request to events table is working

--- src/client/ProfilePage.js
+++ src/client/ProfilePage.js
@@ -4,6 +4,7 @@ import { Button, Typography, InputAdornment, CardContent, Avatar, Box, Card } fr
 import './styles/profile-page.css';
 import { BoltRounded, Search, Description } from '@mui/icons-material'; // default exports vs named exports
 import readEventsRequest from './api/readEventsRequest';
+import dateConverter from './helperFunction';
 
 const ProfilePage = (props) => {
   const navigate = useNavigate();
@@ -19,14 +20,28 @@ const ProfilePage = (props) => {
     };
   }
 
+  const [events, updateEvents] = React.useState([]); // Raw events array
   const [eventsHosting, updateEventsHosting] = React.useState([]); // Raw events array
+  const [eventsAttending, updateEventsAttending] = React.useState([]); // Raw events array
 
   useEffect(() => {
-    // readEventsRequest().then((data)=>{
-    //   console.log(data);
-    //   data = mapEvents(data);
-    //   updateEventsHosting(data)
-    // });
+    readEventsRequest().then((data) => {
+      data = data.sort((a, b) => {
+        let dateA = new Date(dateConverter(a.date_time))
+        let dateB = new Date(dateConverter(b.date_time))
+        return dateA - dateB
+      })
+      console.log(data);
+      updateEvents(data);
+      const eventsHostingData = data.reduce((acc, curr) => {
+        if (curr.host_id === props.currentUser.user_id) {
+          acc.push(curr);
+        }
+        return acc;
+      }, []); 
+      updateEventsHosting(mappingCards(eventsHostingData, 'hosting'));
+    });
+
   }, []);
 
   const editEventButton = (event) => {
@@ -78,32 +93,64 @@ const ProfilePage = (props) => {
     },
   ]
 
-  const eventCards = testEvents.map((event, i) => {
-    return (
-      <div className="cardWrapperProfile" key={i}>
-        <Card sx={{ borderColor: 'rgba(119, 136, 153, 0.5)', borderWidth: 1, borderStyle: 'solid' }}> {/* #778899 with 50% opaqueness represented in rgba */}
-          <React.Fragment>
-            <Box sx={{ height: 15, bgcolor: "#003366" }} />
-            <CardContent className="eventCardContentProfile">
-              <Typography sx={{ fontSize: 14, margin: '0px' }} color="text.secondary" gutterBottom>
-                {event.eventDate}
-              </Typography>
-              <Typography variant="h6" component="div" sx={{ color: '#000000' }}>
-                {event.eventName} {/* This should be a link that shows user more information about event */}
-              </Typography>
-              <Typography sx={{ mb: 1, fontSize: 14 }} color="text.secondary">
-                {event.location} | {event.industry} | {event.eventType}
-              </Typography>
-              <Typography variant="body2" sx={{ fontSize: 14 }}>
-                {event.numAttendees} attendees
-              </Typography>
-              <Button onClick={()=>editEventButton(event)}>Edit Event</Button>
-            </CardContent>
-          </React.Fragment>
-        </Card>
-      </div>
-    );
-  });
+  const mappingCards = (arrayOfData, typeOfEvents) => {
+    if (typeOfEvents === 'hosting') {
+      return arrayOfData.map((event, i) => {
+        return (
+          <div className="cardWrapperProfile" key={i}>
+            <Card sx={{ borderColor: 'rgba(119, 136, 153, 0.5)', borderWidth: 1, borderStyle: 'solid' }}> {/* #778899 with 50% opaqueness represented in rgba */}
+              <React.Fragment>
+                <Box sx={{ height: 15, bgcolor: "#003366" }} />
+                <CardContent className="eventCardContentProfile">
+                  <Typography sx={{ fontSize: 14, margin: '0px' }} color="text.secondary" gutterBottom>
+                    {event.date_time}
+                  </Typography>
+                  <Typography variant="h6" component="div" sx={{ color: '#000000' }}>
+                    {event.event_name} {/* This should be a link that shows user more information about event */}
+                  </Typography>
+                  <Typography sx={{ mb: 1, fontSize: 14 }} color="text.secondary">
+                    {event.event_location} | {event.industry} | {event.event_type}
+                  </Typography>
+                  <Typography variant="body2" sx={{ fontSize: 14 }}>
+                    {event.total_attendees} attendees
+                  </Typography>
+                  <Button onClick={()=>editEventButton(event)}>Edit Event</Button>
+                  <Button onClick={()=>editEventButton(event)}>Delete Event</Button>
+                </CardContent>
+              </React.Fragment>
+            </Card>
+          </div>
+        );
+      })
+    }
+    if (typeOfEvents === 'attended') {
+      return arrayOfData.map((event, i) => {
+        return (
+          <div className="cardWrapperProfile" key={i}>
+            <Card sx={{ borderColor: 'rgba(119, 136, 153, 0.5)', borderWidth: 1, borderStyle: 'solid' }}> {/* #778899 with 50% opaqueness represented in rgba */}
+              <React.Fragment>
+                <Box sx={{ height: 15, bgcolor: "#003366" }} />
+                <CardContent className="eventCardContentProfile">
+                  <Typography sx={{ fontSize: 14, margin: '0px' }} color="text.secondary" gutterBottom>
+                    {event.date_time}
+                  </Typography>
+                  <Typography variant="h6" component="div" sx={{ color: '#000000' }}>
+                    {event.event_name} {/* This should be a link that shows user more information about event */}
+                  </Typography>
+                  <Typography sx={{ mb: 1, fontSize: 14 }} color="text.secondary">
+                    {event.event_location} | {event.industry} | {event.event_type}
+                  </Typography>
+                  <Typography variant="body2" sx={{ fontSize: 14 }}>
+                    {event.total_attendees} attendees
+                  </Typography>
+                </CardContent>
+              </React.Fragment>
+            </Card>
+          </div>
+        );
+      })
+    }
+  }
 
   return (
     <div className="profile-page">
@@ -129,19 +176,19 @@ const ProfilePage = (props) => {
           <Typography sx={{ fontSize: 20, fontWeight: 'bold', margin: '10px' }} className="eventsHostingHeader" gutterBottom>
             Events I'm hosting
           </Typography>
-          {eventCards}
+          {eventsHosting}
         </div>
         <div>
           <Typography sx={{ fontSize: 20, fontWeight: 'bold', margin: '10px' }} className="eventsHostingHeader" gutterBottom>
             Events I'm attending
           </Typography>
-          {eventCards}
+          {eventsHosting}
         </div>
         <div>
           <Typography sx={{ fontSize: 20, fontWeight: 'bold', margin: '10px' }} className="eventsHostingHeader" gutterBottom>
             Events I attended
           </Typography>
-          {eventCards}
+          {eventsHosting}
         </div>
       </div>
     </div>
